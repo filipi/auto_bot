9e2f6e57dc9a535533055b89991e622c4c7fe3a6
Filip Vianna <filipi@pucrs.br>
2017-09-15 14:48:46 -0300

    minor fixes in the case example file

--- case.py
+++ case.py
@@ -9,11 +9,11 @@
 
 file = {}
 
-file[1] {}
+file[1] = {}
 file[1]['name'] = 'file_A.f90' # file to replace parameters
 file[1]['nx'] = 1.             # assign 1. to the variable nx on file_A.f90
 
-file[2]{}
-file[2]['name'] = file_B.f90
+file[2] = {}
+file[2]['name'] = 'file_B.f90'
 file[2]['nx'] = 2.
 

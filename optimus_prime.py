7c846852338fb36a5764baadd87433ade4dfc309
Filipi Vianna <filipi@pucrs.br>
2017-09-19 17:33:03 -0300

    working

--- optimus_prime.py
+++ optimus_prime.py
@@ -7,41 +7,44 @@
 #     Trying to meet Tulio's quality standards ;-)
 # September, 2017
 
-import argparse #https://docs.python.org/2/library/argparse.html
-import os
-import shutil
+import argparse  # https://docs.python.org/2/library/argparse.html
 import fileinput
-import re
 import importlib
-
+import os
+import re
+import shutil
 from subprocess import call
+
 
 ## a small function that prints a string without breaking the line at the end
 ## https://stackoverflow.com/questions/493386/how-to-print-without-newline-or-space
 def printNNL(string):
     import sys
     sys.stdout.write(string)
-    sys.stdout.flush()    
+    sys.stdout.flush()
+
 
 def checkFile(filename, error):
     printNNL('Filename: ' + filename)
     if os.path.isfile(filename):
-        print ' [  OK  ]'
+        print(' [  OK  ]')
     else:
-        print ' [ FAIL ]'
+        print(' [ FAIL ]')
         error = True
     return error
 
+
 ## TODO
-# Add an option to automaticaly generate an example case file.
+# Add an option to automatically generate an example case file.
 
 # First checks if all files are present in source_dir
 # Copy source_dir for each case in cases
 # replace vars in case.file[]
 
-parser = argparse.ArgumentParser(description='Fortran + MPI numerical simulation experiments automated validation tool.')
-parser.add_argument('-c','--cases', metavar='case', nargs='+',
-                    help='<Required> case names whitout .py extension', required=True)
+parser = argparse.ArgumentParser(
+    description='Fortran + MPI numerical simulation experiments automated validation tool.')
+parser.add_argument('-c', '--cases', metavar='case', nargs='+',
+                    help='<Required> case names without .py extension', required=True)
 parser.add_argument('--src', metavar='source_dir', type=str,
                     help='Path for the template simulation (default ./src/)')
 parser.add_argument('--path', metavar='path', type=str,
@@ -54,59 +57,95 @@ if args.path is None:
 if args.src is None:
     args.src = './src'
 
-print 'path: ' + args.path
-print 'source_dir: ' + args.src
+#print('path: ' + args.path)
+#print('source_dir: ' + args.src)
 
 error = False;
 for case in args.cases:
-    print('Checking cases...')
+    #print('Checking cases...')
     error = checkFile(case + '.py', error)
 
     # First check if all the files exist
-    print '#################################################'
-    print 'Case: ' + case
-    print('Checking files...')
-    
+    #print('#################################################')
+    #print('Case: ' + case)
+    #print('Checking files...')
+
     if not error:
-        c = importlib.import_module(case)   
+        c = importlib.import_module(case)
         for i in c.file:
             if c.file[i]['name']:
                 error = checkFile(args.src + '/' + c.file[i]['name'], error)
-        
+
 if error:
     quit()
-        
 
 for case in args.cases:
     path = 'case_' + case
-    print( path )
+    #print(path)
     try:
-        shutil.copytree(args.src, path)    
-    except OSError, e:
+        shutil.copytree(args.src, path)
+#    except OSError, e:
+    except OSError:
         pass
-    
-    os.chdir( path )
+
+    os.chdir(path)
     for i in c.file:
-        print '------------------------------------------------'
-        print 'Filename: ' + c.file[i]['name']
+        #print('------------------------------------------------')
+        #print('Filename: ' + c.file[i]['name'])
         filename = c.file[i]['name']
-        origin = open ( filename, 'r' )
-        content = origin.read()        
+        origin = open(filename, 'r')
+        content = origin.read()
+        #print(content)
+        #quit()
         origin.close()
-        for j in c.file[i]:            
+        for j in c.file[i]:
             if j != 'name':
-                print j + ' = ' + str(c.file[i][j])              
-                content_new = re.sub('(\d.|\d.\.).*\#' + j + '\s', str(c.file[i][j]) +  '.\t#' + j + ' ', content, flags = re.M)
-                destiny = open (filename, 'w' )
-                destiny.write(content_new)
+
+                #print(j + ' = ' + str(c.file[i][j]))
+                #content_new = re.sub('(\d.|\d.\.).*\#' + j + '(\s|\t)', str(c.file[i][j]) + '.\t#' + j + ' ', content, flags=re.M)
+                pattern = '(\d.|\d.\.).*\#' + j + '\s'
+                replace = str(c.file[i][j]) + '.\t#' + j + ' '
+                content_new = re.subn(pattern, replace,  content)
+                re.purge()
+                
+                #print(content_new[1])
+                if (content_new[1] == 0):
+                    pattern = j + '=\d.*?(\n|,)'
+                    replace = j + '=' + str(c.file[i][j])
+                    content_new = re.subn(pattern, replace,  content)
+                    re.purge()
+                    if (content_new[1] == 0):
+                        pattern = j + '=\w.*?(\n)'
+                        replace = j + '=' + str(c.file[i][j])
+                        content_new = re.subn(pattern, replace,  content)
+                        re.purge()
+                        #print (pattern)
+                        #print (replace)
+                        #print(content_new[1])
+                
+                #if j == 'xlx':
+                #    print (pattern)
+                #    print (replace)
+                    #print(content_new[0])
+                    #quit()
+                    
+                #print('Expressao regular para ' + j)
+                #print('(\d.|\d.\.).*\#' + j + '\s' )
+                #print( str(c.file[i][j]) + '.\t#' + j + ' ' )
+                #print('--------------------------------')
+                content = content_new[0]
+                destiny = open(filename, 'w')
+                #print(path)
+                #print(filename)
+                destiny.write(content_new[0])
                 destiny.close
 
-#     ##call(["make", ""])
+# ##call(["make", ""])
 #     ##call(["mpi_rum", ""])
 
-#mkdir caso_1 &&
-#cp -Rf src caso_1 &&
-#cd caso_1 &&
-#sed -ie 's/(\d.\d.*?)\#re/3000\t\#re/g' hello.txt
-#make &&
-#mpi_run .. &&
+# mkdir caso_1 &&
+# cp -Rf src caso_1 &&
+# cd caso_1 &&
+# sed -ie 's/(\d.\d.*?)\#re/3000\t\#re/g' hello.txt
+# make &&
+# mpi_run .. &&
